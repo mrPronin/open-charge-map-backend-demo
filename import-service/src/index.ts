d647788d22ddfafc7f1a3a235d8ffc0adf13e7aa
Oleksandr Pronin <pronin.alx@gmail.com>
2023-05-09 21:43:09 +0200

    refactor: import-service, extract resolvers from index.ts

--- import-service/src/index.ts
+++ import-service/src/index.ts
@@ -1,10 +1,11 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { makeExecutableSchema } from "@graphql-tools/schema";
-import { DateTimeResolver, DateTimeTypeDefinition } from "graphql-scalars";
+import { DateTimeTypeDefinition } from "graphql-scalars";
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { resolvers } from "./resolvers";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,47 +14,6 @@ const typeDefs = fs.readFileSync(
   path.join(__dirname, 'schema.graphql'),
   'utf8'
 );
-
-const mockedImportSessions = [
-  {
-    id: "57BC7CE8-E2C7-46D3-B009-ECFE508DCDF2",
-    poiAmount: 5,
-    modifiedsince: "2023-05-04T00:00:00Z",
-    startDate: "2023-05-07T10:00:00Z",
-    endDate: "2023-05-07T10:01:00Z",
-  },
-  {
-    id: "2FC98C26-0DD1-4D15-8538-DFB31F10C63E",
-    poiAmount: 10,
-    modifiedsince: "2023-05-05T00:00:00Z",
-    startDate: "2023-05-08T10:00:00Z",
-    endDate: "2023-05-08T10:01:00Z",
-  },
-  {
-    id: "ABABE00E-17E0-4ABA-9BF6-2F52E0726C94",
-    poiAmount: 8,
-    modifiedsince: "2023-05-06T00:00:00Z",
-    startDate: "2023-05-09T10:00:00Z",
-    endDate: "2023-05-09T10:01:00Z",
-  },
-];
-
-const mockedImportMutationResponse = {
-  code: "200",
-  success: true,
-  message: "Data has been successfully imported",
-  importSession: mockedImportSessions[0],
-};
-
-const resolvers = {
-  DateTime: DateTimeResolver,
-  Mutation: {
-    import: () => mockedImportMutationResponse,
-  },
-  Query: {
-    importSessions: () => mockedImportSessions,
-  },
-};
 
 const server = new ApolloServer({
   schema: makeExecutableSchema({
