f59d865a583139f0c9f5165def036aff8a2c8e79
Oleksandr Pronin <pronin.alx@gmail.com>
2023-05-12 18:14:31 +0200

    feat: import-service - define mongoose model for Country

---
+++ import-service/src/models/Country.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
+interface ICountry extends Document {
+  _id: string;
+  ID: number;
+  ISOCode: string;
+  ContinentCode: string;
+  Title: string;
+}
+
+const CountrySchema = new Schema<ICountry>(
+  {
+    _id: { type: String, default: uuidv4 },
+    ID: Number,
+    ISOCode: String,
+    ContinentCode: String,
+    Title: String,
+  },
+  { _id: false }
+);
+
+const Country = mongoose.model<ICountry>('Country', CountrySchema);
+
+export default Country;
