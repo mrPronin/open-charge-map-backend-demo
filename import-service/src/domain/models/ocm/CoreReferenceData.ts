64cdfdfb3dfa88fa4cee799a539f93c5a36083a8
Oleksandr Pronin <pronin.alx@gmail.com>
2023-05-15 22:00:44 +0200

    feat: import-service - define CoreReferenceData

--- import-service/src/domain/models/ocm/CoreReferenceData.ts
+++ import-service/src/domain/models/ocm/CoreReferenceData.ts
@@ -1,1 +1,13 @@
-export interface CoreReferenceData {}
+import { LevelType } from './LevelType.js';
+import { ConnectionType } from './ConnectionType.js';
+import { Country } from './Country.js';
+import { OperatorInfo } from './OperatorInfo.js';
+import { StatusType } from './StatusType.js';
+
+export interface CoreReferenceData {
+  ChargerTypes: LevelType[];
+  ConnectionTypes: ConnectionType[];
+  Countries: Country[];
+  Operators: OperatorInfo[];
+  StatusTypes: StatusType[];
+}
