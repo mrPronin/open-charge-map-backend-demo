b95f8d0412dd5868b9107ea272d6894653146876
Oleksandr Pronin <pronin.alx@gmail.com>
2023-05-24 16:25:13 +0200

    feat: client-api - implement relay-style pagination with prisma

--- client-api/src/index.ts
+++ client-api/src/index.ts
@@ -2,21 +2,16 @@ import 'reflect-metadata';
 import { container } from '@presentation/ioc_container.js';
 import { bootstrap } from '@presentation/bootstrap.js';
 import { referenceIoCData } from '@/inversify.config';
-// debug
 // eslint-disable-next-line import/order, import/no-extraneous-dependencies
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-// debug
 
 async function main() {
   const port =
     (process.env.PORT && Number.parseInt(process.env.PORT, 10)) || 4001;
   const { MONGODB_URI, MONGO_DB } = process.env;
   await bootstrap(container, port, MONGODB_URI, MONGO_DB, referenceIoCData);
-
-  const firstTenPOI = await prisma.pOI.findMany({ take: 10 });
-  console.log(firstTenPOI);
 }
 
 main()
--- client-api/src/presentation/resolvers/Query.ts
+++ client-api/src/presentation/resolvers/Query.ts
@@ -1,13 +1,22 @@
 // import { TYPES } from '@domain/types';
 // TODO: import service
 // import { ImportService } from '@domain/interfaces/services/ImportService.js';
-import { GraphQLContext } from '@presentation/GraphQLContext.js';
+import { parsePaginationArgs } from 'prisma-cursor-pagination';
+// import { GraphQLContext } from '@presentation/GraphQLContext.js';
+// debug
+// eslint-disable-next-line import/order, import/no-extraneous-dependencies
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+// debug
 
 export const resolver = {
   Query: {
-    pois: async (_, __, context: GraphQLContext) => {
-      console.log(context);
-      return [];
+    pois: async (_, args /* , context: GraphQLContext */) => {
+      const { findManyArgs, toConnection } = parsePaginationArgs(args);
+      const poi = await prisma.pOI.findMany(findManyArgs);
+      console.log(poi);
+      return toConnection(poi);
       //   const importService = context.container.get<ImportService>(
       //     TYPES.ImportService
       //   );
