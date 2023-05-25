ba572281ab8ac9fdc1ff51a99b791c5c7b3fcc96
Oleksandr Pronin <pronin.alx@gmail.com>
2023-05-25 22:27:35 +0200

    feat: client-api - define validation and error handling for unser input

--- client-api/src/domain/services/OCMService.ts
+++ client-api/src/domain/services/OCMService.ts
@@ -1,5 +1,6 @@
 import { injectable, inject } from 'inversify';
 import { PaginationArgs } from 'prisma-cursor-pagination';
+import { GraphQLError } from 'graphql';
 
 import { TYPES } from '@domain/types.js';
 import { OCMRepository } from '@domain/interfaces/repositories/OCMRepository.js';
@@ -14,6 +15,33 @@ export class OCMServiceImplementation implements OCMService {
   ) {}
 
   pois = async (args: PaginationArgs): Promise<POI[]> => {
+    if (args.first || args.first) {
+      throw new GraphQLError(
+        'Including a value for both the [first] and the [last] parameter is not supported',
+        {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        }
+      );
+    }
+
+    if (args.first < 0) {
+      throw new GraphQLError('[first] parameter must be positive integer', {
+        extensions: {
+          code: 'BAD_USER_INPUT',
+        },
+      });
+    }
+
+    if (args.last < 0) {
+      throw new GraphQLError('[last] parameter must be positive integer', {
+        extensions: {
+          code: 'BAD_USER_INPUT',
+        },
+      });
+    }
+
     return this.ocmRepository.pois(args);
   };
 }
